2e9ab3b4ffaac71a9829f2be907dc9f8e1872c68
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-09-13 15:33:57 -0700

    Updated JS project including functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function test_1(str1) {  //This defines the function and parameters
+    str1 = "This " //This sets a value to the variable
+    str1 += "is easy!" //Concatenates the strings
+    document.getElementById("new").innerHTML = str1; //This changes the HTML text
+}
