5abd75f9641d1345f93e2a4eeff91290ccd94bb5
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-09-13 15:08:37 -0700

    JavaScript upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+var str1 = "\"This dog isn't ours\", he said calmly."; //Define variable
+var str2 = " \"But I think we can keep him\"";  //Define variable
+var str3 = str1 + str2; //Concatenates string
+var x = "sarrah", y = "david", z = "jason", aa = "pizza"; //Define multiple variable and assigns string to each
+var x = x.fontcolor("pink"), y = y.fontcolor("green"), z = z.fontcolor("red"), aa = aa.fontcolor("purple"); //Changes font-color for string
+document.write(aa);  //Print value to page
+window.alert(str3); //Prints value in alert window
+6 * 7; //Expression
